17774783efbbc2bd975cee8e563a242e1df63d76
Patrick Fulghum <patrick.c.fulghum@gmail.com>
2017-11-09 17:16:40 -0500

    Refactor movement algorithm to make it consistent speed and not pause on shot

--- cupface.js
+++ cupface.js
@@ -4,6 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById('ship');
   const context = canvas.getContext('2d');
   const game = {
+    movement: {
+      "left": false,
+      "right": false,
+      "up": false,
+      "down": false,
+    },
     bullets: [],
     startingX: 200,
     startingY: 200,
@@ -13,29 +19,63 @@ document.addEventListener("DOMContentLoaded", () => {
     bulletContext,
     start: function(x, y) {
       this.context.clearRect(0, 0, 600, 400)
+      game.updateShipPosition();
       this.context.fillRect(this.startingX, this.startingY, x, y)
       this.bulletContext.clearRect(0, 0, 600, 400)
       this.bullets.forEach((bullet) => {
-        bullet.x += 1;
-        bullet.y += 1;
+        bullet.x += 3;
+        // bullet.y += 1;
         this.bulletContext.fillRect(bullet.x, bullet.y, bullet.sizeX, bullet.sizeY)
       });
+    },
+    updateShipPosition: () => {
+      if (game.movement.left) {
+        game.startingX -= 3;
+      }
+      if (game.movement.right) {
+        game.startingX += 3;
+      }
+      if (game.movement.up) {
+        game.startingY -= 3;
+      }
+      if (game.movement.down) {
+        game.startingY += 3;
+      }
     },
     sleep: function(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
     },
-    move: async function (direction) {
-      for (var i = 0; i < 24; i++) {
-        await game.sleep(20);
-        if (direction === "left") {
-          game.startingX -= 3;
-        } else if (direction == "right") {
-          game.startingX += 3;
-        } else if (direction == "up") {
-          game.startingY -= 3;
-        } else {
-          game.startingY += 3;
-        }
+    move: function (direction) {
+      if (direction === "left") {
+        game.movement.right ?
+        game.movement.right = false :
+        game.movement.left = true
+      } else if (direction == "right") {
+        game.movement.left ?
+        game.movement.left = false :
+        game.movement.right = true
+      } else if (direction == "up") {
+        game.movement.down ?
+        game.movement.down = false :
+        game.movement.up = true
+      } else {
+        game.movement.up ?
+        game.movement.up = false :
+        game.movement.down = true
+      }
+    },
+    cease: function(direction) {
+      if (direction === "left") {
+        game.movement.left = false;
+      }
+      if (direction === "right") {
+        game.movement.right = false;
+      }
+      if (direction === "up") {
+        game.movement.up = false;
+      }
+      if (direction === "down") {
+        game.movement.down = false;
       }
     },
     shoot: function(x, y, sizeX, sizeY) {
@@ -70,9 +110,10 @@ document.addEventListener("DOMContentLoaded", () => {
     switch (e.keyCode) {
       case 90:
         console.log("z, parry");
-
-      case 9:
-        console.log('tab, bomb');
+        break
+      case 67:
+        console.log('c, bomb');
+        game.shoot(game.startingX + 10 + game.shipX, game.startingY + game.shipY / 3.14159, 25, 25)
         break
       case 16:
         game.shrink()
@@ -101,6 +142,18 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   document.addEventListener("keyup", (e) => {
     switch (e.keyCode) {
+      case 87 || 38:
+        game.cease("up");
+        break
+      case 65 || 37:
+        game.cease('left')
+        break
+      case 83 || 40:
+        game.cease("down")
+        break
+      case 68 || 39:
+        game.cease("right")
+        break
       case 16:
         game.grow();
         break;
